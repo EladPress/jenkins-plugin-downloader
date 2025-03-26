bdf71827d0bd2c9d5c5e7119f90325bc783e0bdc
EladPress <presselad2@gmail.com>
2025-03-26 21:46:52 +0200

    Cleaned up

--- src/process_plugins.py
+++ src/process_plugins.py
@@ -33,6 +33,23 @@ def get_plugin_info(plugin_name, version='all'):
         }]
     return plugin
 
+def get_all_plugins_info():
+    plugin_names = get_plugins_names()
+    print('Retrieving info for all plugins (parallelized)')
+    latest_links = []
+
+    plugins = []
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures = {executor.submit(get_plugin_info, plugin_name): plugin_name for plugin_name in plugin_names}
+
+        with tqdm(total=len(futures), desc="Processing") as pbar:
+            for future in as_completed(futures):
+                plugins += future.result()
+                plugin_name = future.result()[0]['name']
+                pbar.set_postfix({"Current": plugin_name})
+                pbar.update(1)  # Update progress bar as tasks complete
+
+    return plugins
 
 def get_plugins_names():
     print('Retrieving plugins list:')
